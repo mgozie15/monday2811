c489789c2be6bb90290b161e145087467a2b2bc5
mgozie15 <118526371+mgozie15@users.noreply.github.com>
2022-11-28 15:54:45 +0000

    Add files via upload

---
+++ song.js
@@ -0,0 +1,14 @@
+function bottlesofbeer() {
+	for(i=99; i>1; i--) {
+			calc = i - 1
+	document.write ( i + "bottles of beer on the wall,"  +i+  "bottles of beer <br> Take one down and pass it around," +calc+ "bottles of beer on the wall <br> <br>")
+	}
+	{
+	document.write ("No more bottles of beer on the wall, no more bottles of beer <br> Go to the store and buy some more, 99 bottles of beer on the wall")
+	}
+
+
+	
+	
+}
+		
